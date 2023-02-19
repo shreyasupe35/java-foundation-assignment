2b9eec04ff42d925fbdc74958666de235c045aab
shreyasupe35 <115993492+shreyasupe35@users.noreply.github.com>
2023-02-19 11:57:45 +0530

    Delete ass3 directory

--- ass3/ass1.java
+++
@@ -1,9 +0,0 @@
-import java .util.Scanner;
-class ass1{
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int x=sc.nextInt();
-        int y=((x+8)/3)%5*5;
-        System.out.println(y);
-    }
-}
--- ass3/tempCodeRunnerFile.java
+++
@@ -1,2 +0,0 @@
-iz=(ans1^ans2);
-        //  System.out.println(z);
